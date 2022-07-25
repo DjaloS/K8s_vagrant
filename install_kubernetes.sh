d826df0daecd9098858ed7036bb29998fae24d3c
DjaloS <109952373+DjaloS@users.noreply.github.com>
2022-07-25 11:12:39 +0200

    Update install_kubernetes.sh

--- install_kubernetes.sh
+++ install_kubernetes.sh
@@ -8,8 +8,8 @@ yum -y install ansible
 # retrieve ansible code
 yum -y install git
 rm -Rf cursus-devops || echo "previous folder removed"
-git clone https://github.com/diranetafen/cursus-devops.git
-cd cursus-devops/ansible
+git clone https://github.com/DjaloS/K8s_vagrant.git
+cd K8s_vagrant
 ansible-galaxy install -r roles/requirements.yml
 if [ $1 == "master" ]
 then
