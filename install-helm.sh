5eadfa4f704e2de379cd5cc867f4860f0a25c218
DjaloS <109952373+DjaloS@users.noreply.github.com>
2022-07-27 14:49:43 +0200

    Create install-helm.sh

---
+++ install-helm.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+echo "Install Helm"
+curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/master/scripts/get-helm-3
+chmod 700 get_helm.sh
+./get_helm.sh
+
+echo "Add "
+
+helm repo update
