8ce9c75a73cc92952f3a5ed32bc0e7f3a081ff85
DjaloS <109952373+DjaloS@users.noreply.github.com>
2022-09-27 11:18:32 +0200

    Update install-helm.sh

--- install-helm.sh
+++ install-helm.sh
@@ -3,4 +3,5 @@
 echo "Install Helm"
 curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/master/scripts/get-helm-3
 chmod 700 get_helm.sh
+export PATH=/sbin:/bin:/usr/sbin:/usr/bin:/usr/local/bin
 ./get_helm.sh
